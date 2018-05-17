7b079e5371d1d453652a5b66bb7a4f73e7dd2e8e
Nikita Volkov <2545442@gmail.com>
2018-05-17 19:39:56 +0300

    angular test #2

--- angularjs_test/app.js
+++ angularjs_test/app.js
@@ -23,13 +23,58 @@ app.controller("ngrepeatCtrl", function ($scope) {
             MaxLength: 6
         }
     ];
-    //$scope.paramForm.capabilities.$setValidity('required', false);
+	$scope.validCheck = function(form) {
+		//form.capabilities.$setValidity('required', false);
+		//console.log(`${form} ${titl} = ${value}`);
+		//(value < 0) ? console.log(`${title} is INvalid`) : console.log(`${title} is VALID`);
+		//console.log(this.params[0].Value);
+		//console.log(this.params.some(isNegative));
+		if ( this.params.some(isNegative) ) {
+			//form.title.$setValidity('required', false);
+			form.$setValidity('required', false);
+		} else {
+			form.$setValidity('required', true);
+		};
+		//console.log(form);
+		this.params.forEach( function ( item, i ) {
+			let diff = () => {
+				if ( item.Value == null ) {
+					return item.MaxLength;
+				} else {
+					item.MaxLength - item.Value.length;
+				}
+			};
+			let htmlItem = document.getElementsByTagName("INPUT")[i];
+			//console.log(`${i} - ${diff}`);
+			if ( diff < 0 ) {
+				htmlItem.classList.remove("ng-valid");
+				htmlItem.classList.add("ng-invalid");
+			} else if ( diff >= 0 && !(htmlItem.classList.contains("ng-valid")) ) {
+				htmlItem.classList.add("ng-valid");
+				htmlItem.classList.remove("ng-invalid");
+			} else {
+				console.log("hmmmmm...");
+			};
+		});
+	};
 });
 
 
-//paramForm.capabilities.$setValidity("youAreFat", false);
-
-function myf() {
-    paramForm.capabilities.$setValidity('required', false);
+function isNegative(item, i) {
+	//console.log(item.MaxLength - item.Value.length);
+	//item.Value == null ? true : null;
+	//console.log(i);
+	if ( item.Value == null ) {
+		return true;
+	}
+	let diff = item.MaxLength - item.Value.length;
+	if ( diff < 0 ) {
+		let htmlItem = document.getElementsByTagName("INPUT")[i];
+		htmlItem.classList.remove("ng-valid");
+		htmlItem.classList.add("ng-invalid");
+		//console.log(htmlItem);
+	}
+	return diff < 0;
 }
+
 
